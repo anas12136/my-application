368779c646aefaab72e4058f3f77b0868a72b6ba
Anas Sattar <anas.kaarwa@gmail.com>
2021-08-19 18:09:12 +0500

    chapter 19 completed

--- lib/Widgets/home_widgets/catlogList.dart
+++ lib/Widgets/home_widgets/catlogList.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:git_project_2/Models/catlog.dart';
+
 import 'package:git_project_2/Pages/home_detail_page.dart';
 
 import 'catlogItem.dart';
@@ -17,7 +18,7 @@ class CatlogList extends StatelessWidget{
       //itemCount: CatlogModel.items.length,
       itemCount: CatlogModel.items.length,
       itemBuilder:(context, index) {
-        final catlog = CatlogModel.items[index];
+        final catlog = CatlogModel.getByPosition(index);
         return InkWell(
             onTap: () => Navigator.push(
                 context, MaterialPageRoute(
